4453dd00356c3f87c472c9e989500aa7083d805f
Caro <carotonii@icloud.com>
2017-07-18 20:06:10 -0700

    new assigment

---
+++ Basic13/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Basic13
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
